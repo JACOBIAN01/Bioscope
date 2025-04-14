31e873200e8108cb888b64591c20bb6aada1a12e
JACOBIAN01 <subhadeepghorai23@gmail.com>
2025-04-14 16:16:32 +0530

    Update

--- frontend/bioscope/src/pages/WatchMovie.jsx
+++ frontend/bioscope/src/pages/WatchMovie.jsx
@@ -1,13 +1,27 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
+const WatchMovie = () => {
+  const { PlayBackId } = useParams();
 
-
-const WatchMovie = ()=>{
-    return(
-        <div>
-            <h1>Enjoy Movie..</h1>
+  return (
+    <div className="min-h-screen bg-gray-900">
+      <Navbar />
+      <div className="flex justify-center items-center mt-8">
+        <div className="w-full max-w-4xl">
+          <video
+            controls
+            className="w-full rounded-2xl border-4 border-yellow-500"
+          >
+            <source src={PlayBackId} type="application/x-mpegURL">
+                Your Browser does not support the video tag!
+            </source>
+          </video>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default WatchMovie;
