58257f4429459392d0bcd740255e8501a0328c31
JACOBIAN01 <subhadeepghorai23@gmail.com>
2025-04-19 00:38:19 +0530

    Update

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://bioscope-five.vercel.app"],
+    origin: ["http://localhost:5173", "https://bioscope-beta.vercel.app/"],
     credentials: true,
   })
 );
